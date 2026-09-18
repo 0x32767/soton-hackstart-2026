14a3c69ebbd3e664a05afc03644a339ad998e38c
reisensei <hanmanseck@gmail.com>
2026-09-18 16:31:23 +0100

    Added basic infrastructure for navbar

--- src/soton_hackstart_2026/__init__.py
+++ src/soton_hackstart_2026/__init__.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
+default_settings = {
+    "navbar_items": [
+        ["/", "Home"],
+        # ["/example-page", "Example"],
+    ]
+}
+
 @app.route("/")
 def index():
-    return "soton"
+    return render_template("index.html", **default_settings)
 
 def main():
     app.run(host="127.0.0.1", port=8080, debug=True)
