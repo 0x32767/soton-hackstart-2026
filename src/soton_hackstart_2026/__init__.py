744a1527ceccec1ba24e9cc4081b488e02e49676
reisensei <hanmanseck@gmail.com>
2026-09-19 16:29:44 +0100

    Added infinate loop fix

--- src/soton_hackstart_2026/__init__.py
+++ src/soton_hackstart_2026/__init__.py
@@ -22,10 +22,19 @@ def run(text: str, input_func, output_func):
     ]
 
     alive = True
-    while alive:
+    while True:
+        alive = False
+
         for interpreter in interpreters:
-            alive = alive or (not interpreter.terminated)
+            print(interpreter.terminated)
+
+            if not interpreter.terminated:
+                alive = True
+
             interpreter.advance(parser, memory, parser.size, input_func=input_func, output_func=output_func)
+
+        if not alive:
+            return
 
 def main() -> None:
     import soton_hackstart_2026.IDE_GUI
